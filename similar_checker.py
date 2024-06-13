6c9737db69f102b2fdcebc6b29808c428bd5cf4c
Minsu Kim <minsu.steven.kim@gmail.com>
2024-06-13 16:19:35 +0900

    [feature] alphabet similar

--- similar_checker.py
+++ similar_checker.py
@@ -1,2 +1,30 @@
+MAX_ALPHA_SIMILAR = 40
+
+
 class SimilarChecker:
-    pass
+    def __init__(self):
+        pass
+
+    def check_alphabet(self, a, b):
+        a_alphabets = set()
+        b_alphabets = set()
+        for alphabet in a:
+            a_alphabets.add(alphabet)
+        for alphabet in b:
+            b_alphabets.add(alphabet)
+
+        all_alphabets = set()
+        same_count = 0
+        total_count = 0
+        for alphabet in a_alphabets:
+            if alphabet not in all_alphabets:
+                all_alphabets.add(alphabet)
+                total_count += 1
+                if alphabet in b_alphabets:
+                    same_count += 1
+        for alphabet in b_alphabets:
+            if alphabet not in all_alphabets:
+                all_alphabets.add(alphabet)
+                total_count += 1
+
+        return MAX_ALPHA_SIMILAR * same_count // total_count
--- test_similar_checker.py
+++ test_similar_checker.py
@@ -1,6 +1,20 @@
 from unittest import TestCase
 
+from similar_checker import SimilarChecker
+
 
 class TestSimilarChecker(TestCase):
-    def test_placeholder(self):
-        self.assertEqual(1, 1)
+    def setUp(self):
+        super().setUp()
+        self.checker = SimilarChecker()
+
+    def test_alphabet_similar(self):
+        test_cases = [
+            (40, 'ASD', 'DSA'),
+            (0, 'A', 'BB'),
+            (40, 'AAABB', 'BAA'),
+            (20, 'AA', 'AAE')
+        ]
+        for tc in test_cases:
+            with self.subTest(tc):
+                self.assertEqual(tc[0], self.checker.check_alphabet(tc[1], tc[2]))
