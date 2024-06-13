e1f69b8a5f6ddf420aa6da65f8ad32d5e39c027e
Minsu Kim <minsu.steven.kim@gmail.com>
2024-06-13 15:40:02 +0900

    [feature] length similar 0 and 60

--- similar_checker.py
+++ similar_checker.py
@@ -1,2 +1,8 @@
 class SimilarChecker:
-    pass
+    def __init__(self):
+        pass
+
+    def check_length(self, a, b):
+        if len(a) == len(b):
+            return 60
+        return 0
--- test_similar_checker.py
+++ test_similar_checker.py
@@ -1,6 +1,19 @@
 from unittest import TestCase
 
+from similar_checker import SimilarChecker
+
 
 class TestSimilarChecker(TestCase):
-    def test_placeholder(self):
-        self.assertEqual(1, 1)
+    def setUp(self):
+        super().setUp()
+        self.checker = SimilarChecker()
+
+    def test_same_length(self):
+        a = 'ASD'
+        b = 'DSA'
+        self.assertEqual(60, self.checker.check_length(a, b))
+
+    def test_length_similar_0(self):
+        a = 'A'
+        b = 'BB'
+        self.assertEqual(0, self.checker.check_length(a, b))
